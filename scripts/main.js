241322fbdef3e3c931f00942cde905085ab491c3
Ryoung27 <rryoung89@gmail.com>
2022-09-12 20:54:28 -0500

    Assignment One Week Four

---
+++ scripts/main.js
@@ -0,0 +1,78 @@
+// Practice Assignment 1
+// Question 1
+// The variable myNumber should by defined.
+let myNumber;
+
+// Question 2
+// The variable myNumber should be type number.
+myNumber = Number()
+// The variable myNumber should be equal to 5.
+myNumber = 5;
+
+// Question 3
+// The variable myOtherNumber should by defined.
+let myOtherNumber;
+// The variable myOtherNumber should be type number.
+myOtherNumber = Number();
+// The variable myOtherNumber should be equal to 5.
+myOtherNumber = 5;
+
+// Question 4
+// The variable myTotal should by defined.
+let myTotal;
+// The variable myTotal should be type number.
+myTotal = Number();
+// The variable myTotal should be equal to the product of myNumber and myOtherNumber.‣
+myTotal = myNumber + myOtherNumber;
+
+// Question 5
+// The variable myName should by defined.
+let myName;
+// The variable myName should be type string.
+myName = String();
+// The variable myCombo should by defined.
+let myCombo;
+// The variable myCombo should be type string.
+myCombo = String();
+// The variable myCombo should be equal to the sum of myNumber and myOtherNumber.‣
+myCombo = myNumber + myOtherNumber;
+
+// Question 6
+// The variable myOtherCombo should by defined.
+let myOtherCombo;
+// The variable myOtherCombo should be type number.
+myOtherCombo = Number();
+// The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣
+myOtherCombo = myNumber + myOtherNumber;
+
+// Question 7
+// The variable myRemainder should by defined.
+let myRemainder;
+// The variable myRemainder should be type number.
+myRemainder = Number();
+// The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+myRemainder = myNumber % 5;
+
+// Question 8
+// The variable i should by defined.
+let i;
+// The variable i should be type number.
+i = Number();
+// The variable i should be equal to 2.
+i = 2;
+
+// Question 9
+// The variable int should by defined.
+let int;
+// The variable int should be type number.
+int = Number();
+// The variable int should be equal to 4.
+int = 4;
+
+// Question 10
+// The variable myBoolean should by defined.
+let myBoolean;
+// The variable myBoolean should be type boolean.
+myBoolean = Boolean()
+// The variable myBoolean should be false
+myBoolean = false;
